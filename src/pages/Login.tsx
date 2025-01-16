bd8b57233ecb2116ca7f19f46a0cc658620d7195
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-01-17 03:01:40 +0600

    Getting login data

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,32 @@
+import { Button } from 'antd';
+import { useForm } from 'react-hook-form';
+import { useLoginMutation } from '../redux/features/auth/authApi';
+
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const [login, { data, error }] = useLoginMutation();
+  console.log('Data: ', data);
+  console.error(error);
+
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    login(userInfo);
+  };
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor='id'>Id: </label>
+        <input type='text' id='id' {...register('userId')} />
+      </div>
+      <div>
+        <label htmlFor='password'>Password: </label>
+        <input type='text' id='password' {...register('password')} />
+      </div>
+      <Button htmlType='submit'>Login</Button>
+    </form>
   );
 };
 
