7538e925381c4b224800343f8861d91f08744955
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-01-17 14:47:21 +0600

    Login redirect and logout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,15 +1,23 @@
-import { Layout } from 'antd';
+import { Button, Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import { useAppDispatch } from '../../redux/hooks';
+import { logOut } from '../../redux/features/auth/authSlice';
 
 const { Header, Content, Footer } = Layout;
 
 export default function MainLayout() {
+  const dispatch = useAppDispatch();
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
   return (
     <Layout style={{ height: '100vh' }}>
       <Sidebar />
       <Layout>
-        <Header style={{ padding: 0 }} />
+        <Header style={{ padding: 0 }}>
+          <Button onClick={handleLogout}>Logout</Button>
+        </Header>
         <Content style={{ margin: '24px 16px 0' }}>
           <div
             style={{
@@ -21,7 +29,7 @@ export default function MainLayout() {
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          ©{new Date().getFullYear()} Created by Yeasin Rafee
         </Footer>
       </Layout>
     </Layout>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,26 +1,44 @@
 import { Button } from 'antd';
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
 import { useLoginMutation } from '../redux/features/auth/authApi';
 import { useAppDispatch } from '../redux/hooks';
-import { setUser } from '../redux/features/auth/authSlice';
+import { setUser, TUser } from '../redux/features/auth/authSlice';
 import { verifyToken } from '../utils/verifyToken';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 const Login = () => {
+  const navigate = useNavigate();
   const { register, handleSubmit } = useForm();
   const [login] = useLoginMutation();
   const dispatch = useAppDispatch();
 
-  const onSubmit = async (data) => {
-    const userInfo = {
-      id: data.userId,
-      password: data.password,
-    };
-    const res = await login(userInfo).unwrap();
+  const onSubmit = async (data: FieldValues) => {
+    const toastId = toast.loading('Loading...!');
+    try {
+      const userInfo = {
+        id: data.userId,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
 
-    const user = verifyToken(res.data.accessToken);
+      const user = verifyToken(res.data.accessToken) as TUser;
 
-    dispatch(setUser({ user, token: res.data.accessToken }));
-    console.log(res);
+      dispatch(setUser({ user, token: res.data.accessToken }));
+
+      toast.success('User logged in successfully!', {
+        id: toastId,
+        duration: 2000,
+      });
+
+      navigate(`/${user.role}/dashboard`);
+    } catch (err) {
+      toast.error('Something went wrong!', {
+        id: toastId,
+        duration: 2000,
+      });
+      console.log(err);
+    }
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
